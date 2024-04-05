f005b1fcbda84567c26ae0d41e5b8e9087f31577
runner855 <runner855@icloud.com>
2024-04-06 01:04:17 +0200

    retrieving data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import ApiCall from "./API/ApiCall";
 
 export const App = () => {
+  useEffect(() => {
+    ApiCall.get(`/photos/?client_id=${process.env.REACT_APP_API_KEY}`, {}).then(
+      (res) => console.log(res)
+    );
+  });
   return <div className="App">Offline</div>;
 };
