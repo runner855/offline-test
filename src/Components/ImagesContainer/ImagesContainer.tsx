57e9763f58fb80c803bf2c3c7d83be97b1cd4998
runner855 <runner855@icloud.com>
2024-04-09 22:29:41 +0200

    styling images with cards and adding icons

--- src/Components/ImagesContainer/ImagesContainer.tsx
+++ src/Components/ImagesContainer/ImagesContainer.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { ImagesDataStructure } from "../../Types/appTypes";
+import { Card } from "antd";
+import { FaRegHeart } from "react-icons/fa";
+import { AiFillLike } from "react-icons/ai";
+import { LiaCommentSolid } from "react-icons/lia";
 import "../ImagesContainer/ImagesContainer.css";
 
 type ImagesContainerProps = {
   images: ImagesDataStructure[];
 };
+
+const { Meta } = Card;
 
 export const ImagesContainer = ({ images }: ImagesContainerProps) => {
   return (
@@ -14,7 +20,21 @@ export const ImagesContainer = ({ images }: ImagesContainerProps) => {
         images.map((item, index) => {
           return (
             <div className="single_image" key={index}>
-              <img src={item.urls.thumb} alt={item.alt_description} />
+              <Card
+                style={{ width: 300 }}
+                cover={<img alt={item.alt_description} src={item.urls.full} />}
+                actions={[
+                  <div>
+                    <AiFillLike />
+
+                    {item.likes}
+                  </div>,
+                  <FaRegHeart />,
+                  <LiaCommentSolid />,
+                ]}
+              >
+                <Meta description={item.alt_description} />
+              </Card>
             </div>
           );
         })}
